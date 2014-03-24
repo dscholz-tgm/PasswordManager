5f119a5f533733122aec4e2691e5a04127845c81
dscholz-tgm <dscholz@student.tgm.ac.at>
2014-03-24 10:28:23 +0100

    Formatting the Encryption Classes (4 Spaces instead of Tab)

--- src/pwm/engine/algorithms/encryptionalgorithms/AES128Encryption.java
+++ src/pwm/engine/algorithms/encryptionalgorithms/AES128Encryption.java
@@ -1,8 +1,6 @@
 package pwm.engine.algorithms.encryptionalgorithms;
 
-import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
@@ -17,81 +15,71 @@ import pwm.engine.algorithms.hashingalgorithms.HashingAlgorithm;
 
 /**
  * AES: Advanced Encryption Standard-128
- * @author Adrian Bergler
- * @version 0.1
+ *
+ * @author Adrian Bergler, Dominik Scholz
+ * @version 0.2
  */
 /*
  * TODO: Description! (how the algorithm works etc.)
  */
-public class AES128Encryption extends EncryptionAlgorithm{
-	
-	private final int KEYLENGTH = 16;
-	
-	public AES128Encryption(HashingAlgorithm hashAlg) {
-		super(hashAlg);
-	}
+public class AES128Encryption extends EncryptionAlgorithm {
 
-	@Override
-	public byte[] encrypt(byte[] data, byte[] key) throws PWMException{
-		try {
-			//Hashing the key
-			key = hashAlg.hash(key);
-			
-			if(key.length < KEYLENGTH){
-				throw new PWMException("Invalid hashlength!");
-			}
-			
-			key = Arrays.copyOf(key,16);
-			
-			/*
-			 * You could probably extend this to make it easier to adapt by adding the KEYLENGTH to the interface
-			 * and initialize it in the constructor. You could then add a "prepareHash"-method to the interface that works dynamically using the KEYLENGTH.
-			 */
-			
-			//Preparing the algorithm
-			Cipher cipher = Cipher.getInstance("AES");	// AESCBC/NoPadding
-			SecretKeySpec k = new SecretKeySpec(key, "AES");
-			
-			//Setting the algorithm to encryption mode
-			cipher.init(Cipher.ENCRYPT_MODE, k);
-			
-			//Encrypt!
-			byte[] encryptedData = cipher.doFinal(data);
-			
-			return encryptedData;
+    private final Cipher cipher;
+    private final int KEYLENGTH = 16;
 
-		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException ex) {
-			throw new PWMException(ex.getMessage());
-		}
-	}
+    public AES128Encryption(HashingAlgorithm hashAlg) throws PWMException {
+        super(hashAlg);
+        try {
+            cipher = Cipher.getInstance("AES"); // AESCBC/NoPadding
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException ex) {
+            throw new PWMException(ex);
+        }	
+        
+    }
 
-	@Override
-	public byte[] decrypt(byte[] data, byte[] key) throws PWMException {
-		try {
-			//Hashing the key
-			key = hashAlg.hash(key);
-			
-			if(key.length < KEYLENGTH){
-				throw new PWMException("Invalid hashlength!");
-			}
-			
-			key = Arrays.copyOf(key, 16); //TODO REMOVE LATER. This is just to cut the SHA1 hash to 128bit.
-			
-			//Preparing the algorithm
-			Cipher cipher = Cipher.getInstance("AES"); // AESCBC/NoPadding
-			SecretKeySpec k = new SecretKeySpec(key, "AES");
-			
-			//Setting the algorithm to decryption mode
-			cipher.init(Cipher.DECRYPT_MODE, k);
-			
-			//Decrypt!
-			byte[] decryptedData = cipher.doFinal(data);
-			
-			return decryptedData;
+    @Override
+    public byte[] encrypt(byte[] data, byte[] key) throws PWMException {
+        try {
+            key = hashAlg.hash(key);
+            if (key.length < KEYLENGTH) {
+                throw new PWMException("Invalid hashlength!");
+            }
+            key = Arrays.copyOf(key, 16);
+            SecretKeySpec k = new SecretKeySpec(key, "AES");
+            cipher.init(Cipher.ENCRYPT_MODE, k);
+            byte[] encryptedData = cipher.doFinal(data);
+            return encryptedData;
+        } catch (IllegalBlockSizeException | BadPaddingException | InvalidKeyException ex) {
+            throw new PWMException(ex);
+        }
+    }
 
-		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException ex) {
-			throw new PWMException(ex.getClass() + ex.getMessage());
-		}
-	}
-	
+    @Override
+    public byte[] decrypt(byte[] data, byte[] key) throws PWMException {
+        try {
+            //Hashing the key
+            key = hashAlg.hash(key);
+
+            if (key.length < KEYLENGTH) {
+                throw new PWMException("Invalid hashlength!");
+            }
+
+            key = Arrays.copyOf(key, 16); //TODO REMOVE LATER. This is just to cut the SHA1 hash to 128bit.
+
+            //Preparing the algorithm
+            SecretKeySpec k = new SecretKeySpec(key, "AES");
+
+            //Setting the algorithm to decryption mode
+            cipher.init(Cipher.DECRYPT_MODE, k);
+
+            //Decrypt!
+            byte[] decryptedData = cipher.doFinal(data);
+
+            return decryptedData;
+
+        } catch (IllegalBlockSizeException | BadPaddingException | InvalidKeyException ex) {
+            throw new PWMException(ex);
+        }
+    }
+
 }
